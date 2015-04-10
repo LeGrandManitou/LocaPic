9d9ba3fba24c4d3049d0a1d9016b59e78553a25d
LeGrandManitou <v28282828@hotmail.fr>
2015-04-10 13:46:20 +0200

    Ajout licence

--- src/fr/rt/acy/locapic/PreferencesActivity.java
+++ src/fr/rt/acy/locapic/PreferencesActivity.java
@@ -1,3 +1,20 @@
+/**
+ * LocaPic
+ * Copyright (C) 2015  Virgile Beguin and Samuel Beaurepaire
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.rt.acy.locapic;
 
 import android.app.Activity;
@@ -18,7 +35,7 @@ public class PreferencesActivity extends Activity {
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        // Charge les preferences depuis le layout preference_fragment
-	        // Ce dernier contient pour chaque champ, sa clé (chaine de caracteres) pour les preferences
+	        // Ce dernier contient pour chaque champ, sa clÃ© (chaine de caracteres) pour les preferences
 	        addPreferencesFromResource(R.layout.preference_fragment);
 	    }
 	}
